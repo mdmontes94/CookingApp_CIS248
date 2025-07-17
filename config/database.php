2e954f9f81ab7504291cc52a7532ddb9c3b82d29
mdmontes94 <156222980+mdmontes94@users.noreply.github.com>
2025-07-16 22:55:08 -0500

    Added getConnection() function to database.php sans user and password

--- config/database.php
+++ config/database.php
@@ -1,3 +1,18 @@
 <?php
 
+function getConnection() {
+    $dataURL = 'mysql:host=localhost;dbname=cookingappdb';
+    $user = '';
+    $pass = '';
+
+    try {
+        $db = new PDO($dataURL, $user, $pass);
+        return $db;
+    } catch(PDOException $e) {
+        $error = $e->getMessage();
+        echo "<p>Connection Error: $error </p>";
+        return null;
+    }
+}
+
 ?>
